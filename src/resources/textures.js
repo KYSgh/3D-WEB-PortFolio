8a8e7cfc1f897ca36d74f9774fe9fc2439e43613
KYSgh <162289300+KYSgh@users.noreply.github.com>
2024-12-09 15:09:07 +0900

    Add files via upload

---
+++ src/resources/textures.js
@@ -0,0 +1,64 @@
+//billboardTextures
+let billboardTextures = {};
+billboardTextures.terpSolutionsTexture = '3D-WEB-PortFolio/src/jsm/zootopic.gif';
+// terp >> zoo topic
+billboardTextures.bagHolderBetsTexture = '3D-WEB-PortFolio/src/jsm/Bagholdersbetsbillboard.png';
+// > bagholder Bets > Linked In
+billboardTextures.homeSweetHomeTexture = '3D-WEB-PortFolio/src/jsm/home-sweet-home-portrait.png';
+// > kansai <予定
+
+//box textures
+let boxTexture = {};
+boxTexture.Github = '3D-WEB-PortFolio/src/jsm/githubLogo.png';
+boxTexture.twitter = '3D-WEB-PortFolio/src/jsm/twitter.png';
+boxTexture.LinkedIn = '3D-WEB-PortFolio/src/jsm/linkedInLogo.png';
+//linked in
+boxTexture.mail = '3D-WEB-PortFolio/src/jsm/envelope.png';
+boxTexture.globe = '3D-WEB-PortFolio/src/jsm/thunder.png';
+boxTexture.reactIcon = '3D-WEB-PortFolio/src/jsm/react.png';
+boxTexture.allSkills = '3D-WEB-PortFolio/src/jsm/allSkills.png';
+boxTexture.lensFlareMain = '3D-WEB-PortFolio/src/jsm/lensflare0.png';
+boxTexture.skrillex = '3D-WEB-PortFolio/src/jsm/skrillex.png';
+boxTexture.edmText = '3D-WEB-PortFolio/src/jsm/EDM.png';
+
+//material textures
+let stoneTexture = '3D-WEB-PortFolio/src/jsm/stone.png';
+let woodTexture = '3D-WEB-PortFolio/src/jsm/woodTexture.jpg';
+
+//text
+let inputText = {};
+inputText.terpSolutionsText = '3D-WEB-PortFolio/src/jsm/terp-solutions-text.png';
+// terp >> zootopic
+inputText.activities = '3D-WEB-PortFolio/src/jsm/activities_text.png';
+inputText.certification = '3D-WEB-PortFolio/src/jsm/certification.png';
+inputText.redhat = '3D-WEB-PortFolio/src/jsm/redhat.png';
+inputText.bagholderBetsText = '3D-WEB-PortFolio/src/jsm/bagholderbets-text.png';
+// > bagholder Bets >  poke face
+inputText.homeSweetHomeText = '3D-WEB-PortFolio/src/jsm/text.png';
+// > kansai <予定
+
+inputText.staticPortfolio = '3D-WEB-PortFolio/src/jsm/static-portfolio.png';
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '3D-WEB-PortFolio/src/jsm/react-svg.svg';
+
+//URLs
+let URL = {};
+URL.terpsolutions = 'https://velog.io/@kys/%ED%81%AC%EB%A1%A4%EB%A7%81%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EB%89%B4%EC%8A%A4-%EA%B5%AC%EB%8F%85-%EC%9B%B9-%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%A0%9C%EC%9E%91';
+// terp > zoo topic
+URL.bagholderBets = 'https://velog.io/@kys/%EC%96%BC%EA%B5%B4-%EB%AA%AC%EC%8A%A4%ED%84%B0';
+// > bagholder Bets >  poke face
+URL.homeSweetHomeURL = '';
+// > kansai <予定
+URL.gitHub = 'https://github.com/KYSgh';
+URL.twitter = 'https://velog.io/@kys/posts';
+URL.email = 'https://mailto:xfloyd.eth@gmail.com';
+URL.githubBagholder = 'https://velog.io/@kys/%EC%9B%B9-%EC%84%9C%EB%B9%84%EC%8A%A4-CICD';
+URL.LinkedIn = 'https://www.linkedin.com/in/ys-k-38242831a/';
+// > bagholder Bets > poke face
+URL.githubHomeSweetHome = '';
+// > kansai <予定
+
+
+export { billboardTextures, boxTexture, inputText, URL, stoneTexture, woodTexture };
